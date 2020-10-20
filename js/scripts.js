0f80b509e6b014d23da89e068da2524d0e300d0c
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-20 14:51:01 -0700

    add buttons to talk to the computer
    Co-Authored:Ryland Adams <rylandadams@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -18,5 +18,20 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("black-background");
     
- });
+  });
+
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+  });
 });
